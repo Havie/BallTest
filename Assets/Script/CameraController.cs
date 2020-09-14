2af8cade83041a9f96df97712d67d4de23c25d3c
Havie <sfdatz@live.com>
2020-09-13 19:38:05 -0500

    Obi

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform _currBall;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        if(_currBall)
+            _offset = this.transform.position - _currBall.position;
+    }
 
     private void LateUpdate()
     {
-        if(_currBall)
-            this.transform.LookAt(_currBall);
+        if (_currBall)
+            StayInView();
+    }
+
+    public void StayInView()
+    {
+        this.transform.position = _currBall.position + _offset;
+        this.transform.LookAt(_currBall);
+        
     }
 }
